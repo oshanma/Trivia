09555800723f8fc310b4b86675012a3cb1db9d1b
Oshan Maharjan <oshanmaharjan23@gmail.com>
2025-03-15 15:54:50 -0400

    Initial Commit

---
+++ Econ97/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Econ97
+//
+//  Created by Oshan Maharjan on 3/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
